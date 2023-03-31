1399c3815a6d9ae18b95761171f973cf3bcd95f5
Guilherme Braga <guib545@aluno.unb.br>
2023-03-31 11:43:56 -0300

    a

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 
 '''
+> MOXFIELD
 Count -> 1 -> quantas cartas dessa vc tem?
 Tradelist Count	-> 1
 Name	
@@ -16,8 +17,26 @@ Collector Number -> id da carta, integer
 Alter -> "FALSE"	
 Proxy -> "FALSE"	
 Purchase Price -> empty
-''''
+'''
 
 df_ligamagic = pd.read_csv(os.path.dirname(__file__) + "/padrao_liga.csv",  encoding='latin-1')
 
-print(df_ligamagic.head())
+print("Colunas do formato da Ligamagic")
+print(list(df_ligamagic.columns))
+
+'''
+> LIGAMAGIC
+Edicao (PTBR)
+Edicao (EN)
+Edicao (Sigla)
+Card (PT)
+Card (EN)
+Quantidade
+Qualidade (M NM SP MP HP D)
+Idioma (BR EN DE ES FR IT JP KO RU TW)
+Raridade (M R U C)
+Cor (W U B R G M A L) 
+Extras
+Card #
+Comentario
+'''
